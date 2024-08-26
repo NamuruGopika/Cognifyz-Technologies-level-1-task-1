feeee4e6833511bd980896be95702e67e6a003d7
NamuruGopika <namurugopika@gmail.com>
2024-08-26 14:06:28 +0530

    Add files via upload

---
+++ string reversal.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Example usage:
+input_str = "This is my first task in cognify technology"
+reversed_str = reverse_string(input_str)
+print(reversed_str) 
